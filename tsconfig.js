3dc12dc41c8778a9f9b6d77a007016a320bf7120
Kyyyu <mrizkii254500@gmail.com>
2026-08-08 18:38:19 +0700

    Create tsconfig.js

---
+++ tsconfig.js
@@ -0,0 +1,13 @@
+{
+  "compilerOptions": {
+    "target": "ES2022",
+    "module": "NodeNext",
+    "moduleResolution": "NodeNext",
+    "lib": ["ES2022", "DOM"],
+    "esModuleInterop": true,
+    "strict": true,
+    "skipLibCheck": true,
+    "forceConsistentCasingInFileNames": true
+  },
+  "include": ["api/**/*"]
+}
